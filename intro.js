574b248aadbde43b4941b38de5712be0346f3bdc
Edward Mendoza <edcmendoza94@gmail.com>
2017-05-14 18:25:25 -0500

    Second part of JavaScript assignment completed--FizzBuzz program written

--- intro.js
+++ intro.js
@@ -1,3 +1,29 @@
-var jsAssignPt1 = "Introduction to JavaScript";
+/*var jsAssignPt1 = "Introduction to JavaScript";
 
-console.log(jsAssignPt1);
+console.log(jsAssignPt1);*/
+
+//For numbers divisible by 3, print "Fizz" instead
+//For numbers divisible by 5, print "Buzz" instead
+//For numbers divisible by 3 and 5, print "FizzBuzz" instead
+var jsAssignPt2 = 1;
+
+while(jsAssignPt2 <= 100) {
+    if(jsAssignPt2 % 3 === 0 &&
+       jsAssignPt2 % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    
+    else if(jsAssignPt2 % 3 === 0) {
+        console.log("Fizz");
+    }
+    
+    else if(jsAssignPt2 % 5 === 0) {
+        console.log("Buzz");
+    }
+    
+    else {
+        console.log(jsAssignPt2);
+    }
+    
+    jsAssignPt2++;
+}
